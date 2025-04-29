6d80975df91185a62a2c1f162a89bd947095443a
Pitra Pamungkas <pitra.adhi@gmail.com>
2025-04-29 10:01:37 +0700

    add cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+  }),
+  it('Finds the content "type"', () => {
+    expect(true).to.equal(false)
+  })
+})
+
